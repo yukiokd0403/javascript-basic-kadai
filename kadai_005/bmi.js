fc2e4ef094cc3935263d7f6b08ae4d77a1e0d69a
yukiokd0403 <gangtianyushu14@gmail.com>
2024-07-26 13:47:03 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,23 +1,8 @@
-let taiJu;
-taiJu='体重';
-let taiJukg = 68;
-
+let taiJu = 68;
 console.log(taiJu);
-console.log(taiJukg);
 
-let sinChou;
-sinChou='身長';
-sinChoum=1.7;
-
+let sinChou=1.7;
 console.log(sinChou);
-console.log(sinChoum);
 
-let bieMuai;
-bieMuai='bmi';
-console.log(bieMuai);
-
-let keisansiki;
-keisansiki='[体重(kg)]÷[身長(m)×身長(m)]';
-keisansikii=68/(1.7*1.7);
+let keisansiki=68/(1.7*1.7);
 console.log(keisansiki);
-console.log(keisansikii);
