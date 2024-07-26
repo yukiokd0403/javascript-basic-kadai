1ea38d722047c2066a5bfcc5fa02c8ca6a282fe1
yukiokd0403 <gangtianyushu14@gmail.com>
2024-07-26 11:00:29 +0900

    Add files via upload

---
+++ kadai_005/kadai_005/bmi.js
@@ -0,0 +1,22 @@
+// 変数の宣言
+let userName;
+
+// 値の代入
+userName='体重';
+
+// 変数の宣言・値の代入
+let userNumber = 68;
+
+console.log(userName);
+console.log(userNumber);
+
+userName='身長';
+userNumber=1.7;
+
+console.log(userName);
+console.log(userNumber);
+
+userName='IBM';
+userNumber=20;
+console.log(userName);
+console.log(userNumber);
