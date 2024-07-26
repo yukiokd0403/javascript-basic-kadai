8d04ce6d198cc5f9849a9d5dcc90a368dbd9788d
yukiokd0403 <gangtianyushu14@gmail.com>
2024-07-26 11:55:45 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,24 +1,23 @@
-// 変数の宣言
-let userName;
+let taiJu;
+taiJu='体重';
+let taiJukg = 68;
 
-// 値の代入
-userName='体重';
+console.log(taiJu);
+console.log(taiJukg);
 
-// 変数の宣言・値の代入
-let userNumber = 68;
+let sinChou;
+sinChou='身長';
+sinChoum=1.7;
 
-console.log(userName);
-console.log(userNumber);
+console.log(sinChou);
+console.log(sinChoum);
 
-userName='身長';
-userNumber=1.7;
+let bieMuai;
+bieMuai='bmi';
+console.log(bieMuai);
 
-console.log(userName);
-console.log(userNumber);
-
-userName='bmi';
-console.log(userName);
-userName='[体重(kg)]÷[身長(m)×身長(m)]';
-userNumber=68/(1.7*1.7);
-console.log(userName);
-console.log(userNumber);
+let keisansiki;
+keisansiki='[体重(kg)]÷[身長(m)×身長(m)]';
+keisansikii=68/(1.7*1.7);
+console.log(keisansiki);
+console.log(keisansikii);
